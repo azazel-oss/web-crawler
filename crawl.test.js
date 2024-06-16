b2a82d6da8a3bcc25ac0c0fe04c898952d22de4e
Asad Mahmood <asadmhmd1234@gmail.com>
2024-06-16 17:51:06 +0530

    add jsdom dependancies and write tests for both absolute urls and relative urls

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { normalizeURL } from "./crawl";
+import { normalizeURL, getURLsFromHTML } from "./crawl";
 
 describe("tests with http codes", () => {
   test("string that ends with /", () => {
@@ -34,5 +34,104 @@ describe("tests with https codes", () => {
     expect(normalizeURL("https://blog.boot.dev/path/another")).toBe(
       "blog.boot.dev/path/another",
     );
+  });
+});
+
+describe("test processing the urls from the html body", () => {
+  test("html body with single absolute url has length 1", () => {
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toHaveLength(1);
+  });
+  test("html body with single absolute url", () => {
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toContain("https://blog.boot.dev/");
+  });
+  test("html body with single relative url has length 1", () => {
+    expect(
+      getURLsFromHTML(
+        `
+      <html>
+          <body>
+              <a href="/path"><span>Go to Boot.dev</span></a>
+          </body>
+      </html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toHaveLength(1);
+  });
+  test("html body with single relative url", () => {
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="/path"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toHaveLength(1);
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="/path"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toContain("https://blogs.boot.dev/path");
+  });
+  test("html body with multiple urls", () => {
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="/path"><span>Go to Boot.dev</span></a>
+        <a href="https://blog.boot.dev/path/another"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toHaveLength(2);
+    expect(
+      getURLsFromHTML(
+        `
+<html>
+    <body>
+        <a href="/path"><span>Go to Boot.dev</span></a>
+        <a href="https://blog.boot.dev/path/another"><span>Go to Boot.dev</span></a>
+    </body>
+</html>
+`,
+        "https://blogs.boot.dev",
+      ),
+    ).toContain("https://blog.boot.dev/path/another");
   });
 });
